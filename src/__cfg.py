b1deead2659c99c7607ab47cb03ee98d8d6278aa
Kian Wei <kian_wei5@hotmail.com>
2013-04-30 16:40:36 +0800

    Fix config

--- src/__cfg.py
+++ src/__cfg.py
@@ -17,11 +17,13 @@ INSTALLED_PACKAGES = [
 ]
 
 #mongodb
-MONGO_URI = 'mongodb://localhost/unifide'
+MONGO_URI = 'mongodb://kianwei:thisisnotsecureatall@localhost/unifide-backend'
+#MONGO_URI = 'mongodb://localhost/unifide'
 LOG_FILE = os.path.join(cwd, "log.txt")
 MONGO_HOST = "localhost"
 MONGO_PORT = 27017
-MONGO_DB = "unifide"
+MONGO_DB = "unifide-backend"
+#MONGO_DB = "unifide"
 if MOCK_MODE:
     MONGO_DB = "mock"
 
@@ -31,20 +33,21 @@ MOCK_DATE_RANGE_DAYS = 100
 #facebook app info
 FB_APP_ID = "336489173138188"
 FB_APP_SECRET = "b259ee37fdb0acb61feb1b1af0fdab57"
-FB_REDIRECT_URI = "http://127.0.0.1:3000/"
+FB_REDIRECT_URI = "http://127.0.0.1:3000/account/auth/"
 FB_PERMS = ["manage_pages", "publish_stream"]
 FB_REALTIME_TOKEN = "thisisnotsecureatall1987"
 
 #twitter app info
 TW_CONSUMER_KEY = "dB4v3wKk52LIRz6SRSZTJQ"
 TW_CONSUMER_SECRET = "IkfpGDZteinlSBY3iNKwrtuEarB6sZA8U6rbZXJ7h4U"
-TW_REDIRECT_URI = "http://127.0.0.1:3000/"
-ADD_USER_MAX_TWEET = 800
+TW_REDIRECT_URI = "http://127.0.0.1:3000/account/auth/twitter/"
+TW_INDIVIDUAL_STREAM = True
+ADD_USER_MAX_TWEET = 50
 
 #foursquare app info
 FSQ_CLIENT_ID = "DMJY0SMIR5050PMBOL2JKLQWO2R4NCMILNSHMM5VBDJH2AQH"
 FSQ_CLIENT_SECRET = "0EYFVQNM2KTF4V11BS2PZKOPWV2DOBUG1GUHV3D5HOH0SPQO"
-FSQ_REDIRECT_URI = "http://127.0.0.1:3000/"
+FSQ_REDIRECT_URI = "http://127.0.0.1:3000/account/auth/foursquare/"
 
 # API Packages to be enabled
 API_TO_REGISTER = (
