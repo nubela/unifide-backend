f6fd2cf3d404daa10d60c007de8cce781478b508
nubela <nubela@gmail.com>
2013-05-05 16:03:41 +0800

    updated menu

--- src/unifide_backend/action/cp/action.py
+++ src/unifide_backend/action/cp/action.py
@@ -1,42 +1,43 @@
 from unifide_backend.action.cp.model import CPMenu, CPMenuItem
 from unifide_backend.action.admin.user.model import User
 
+
 BASE_MENU = [
-            {"order": 0,
-             "name": "OVERVIEW",
-             "sub-menu":
-                  [
-                      {"name": "View All", "link": "/", "order": "0"},
-                      {"name": "divider", "link": "", "order": "1"},
-                      {"name": "Facebook", "link": "/facebook/page/activity", "order": "2"},
-                      {"name": "Twitter", "link": "/twitter/activity", "order": "3"},
-                      {"name": "Foursquare", "link": "/foursquare/venue/activity", "order": "4"},
-                      {"name": "Brand Mention", "link": "/brand-mention", "order": "5"},
-                      {"name": "divider", "link": "", "order": "6"},
-                      {"name": "Web", "link": "/web-platform", "order": "7"},
-                      {"name": "iOS", "link": "/ios-platform", "order": "8"},
-                      {"name": "Android", "link": "/android-platform", "order": "9"}
-                   ]},
-            {"order": 1,
-             "name": "CAMPAIGN",
-             "sub-menu":
-                  [
-                      {"name": "New Promotion", "link": "/campaign/promo/new", "order": "0"},
-                      {"name": "New Event", "link": "/campaign/event/new", "order": "1"},
-                      {"name": "divider", "link": "", "order": "2"},
-                      {"name": "Manage", "link": "/campaign", "order": "3"}
-                  ]},
-            {"order": 2,
-             "name": "MODULES",
-             "sub-menu":
-                  [
-                      {"name": "Business Info", "link": "/bizinfo", "order": "0"},
-                      {"name": "Blog", "link": "/blog", "order": "1"},
-                      {"name": "Reservations", "link": "/reservation", "order": "2"},
-                      {"name": "divider", "link": "", "order": "3"},
-                      {"name": "Items", "link": "/items", "order": "4"}
-                  ]}
-            ]
+    {"order": 0,
+     "name": "OVERVIEW",
+     "sub-menu":
+         [
+             {"name": "View All", "link": "/", "order": "0"},
+             {"name": "divider", "link": "", "order": "1"},
+             {"name": "Facebook", "link": "/facebook/page/activity", "order": "2"},
+             {"name": "Twitter", "link": "/twitter/activity", "order": "3"},
+             {"name": "Foursquare", "link": "/foursquare/venue/activity", "order": "4"},
+             {"name": "Brand Mention", "link": "/brand-mention", "order": "5"},
+             {"name": "divider", "link": "", "order": "6"},
+             {"name": "Web", "link": "/web-platform", "order": "7"},
+             {"name": "iOS", "link": "/ios-platform", "order": "8"},
+             {"name": "Android", "link": "/android-platform", "order": "9"}
+         ]},
+    {"order": 1,
+     "name": "CAMPAIGN",
+     "sub-menu":
+         [
+             {"name": "New Promotion", "link": "/campaign/promo/new", "order": "0"},
+             {"name": "New Event", "link": "/campaign/event/new", "order": "1"},
+             {"name": "divider", "link": "", "order": "2"},
+             {"name": "Manage", "link": "/campaign", "order": "3"}
+         ]},
+    {"order": 2,
+     "name": "MODULES",
+     "sub-menu":
+         [
+             {"name": "Business Info", "link": "/bizinfo", "order": "0"},
+             {"name": "Blog", "link": "/blog", "order": "1"},
+             {"name": "Orders", "link": "/order", "order": "2"},
+             {"name": "divider", "link": "", "order": "3"},
+             {"name": "Items", "link": "/items", "order": "4"}
+         ]}
+]
 
 
 def init_cp_menu():
