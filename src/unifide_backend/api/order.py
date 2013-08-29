99ff868a13d51b2fe5934e559cd106d25bc3f60c
nubela <nubela@gmail.com>
2013-08-30 01:40:48 +0800

    order creation api

--- src/unifide_backend/api/order.py
+++ src/unifide_backend/api/order.py
@@ -1,39 +1,51 @@
-from flask import jsonify, request, render_template
+from flask import request, jsonify, json
+from base.util import coerce_bson_id
 import orders
+from orders.order.support import apply_shipping, apply_coupon, apply_tax, apply_discounts
 
 
-def get_order():
+def put_order():
     """
-    (GET: order)
+    (PUT: order)
     """
-    all_orders = orders.Order.collection().find({})
-    all_order_objs = [orders.Order.unserialize(dic) for dic in all_orders]
-    order_lis = [x.serialize(json_friendly=True) for x in all_order_objs]
+    items_lis = json.loads(request.form.get("items"))
+    admin_notes = request.form.get("admin_notes")
+    user_notes = request.form.get("user_notes")
+    status = request.form.get("status")
+    user_id = request.form.get("user_id")
+    admin_id = request.form.get("admin_id")
+
+    #actionables
+    apply_shipping_id = request.form.get("shipping_method", None)
+    apply_coupon_code = request.form.get("apply_coupon", None)
+    apply_debits_credits = request.form.get("apply_debits_credits", False)
+    if apply_debits_credits == "false":
+        apply_coupon_code = False
+    elif apply_debits_credits == "true":
+        apply_coupon_code = True
+
+    o = orders.Order()
+    o.user_id = user_id
+    o.status = status
+    o.items = [{"obj_id": coerce_bson_id(x["obj_id"]), "quantity": float(x["quantity"])} for x in items_lis]
+    o.request_notes = user_notes
+    o.admin_notes = admin_notes
+    o.admin_id = admin_id
+    if apply_shipping_id is not None and len(apply_shipping_id) > 0:
+        apply_shipping(o, apply_shipping_id)
+    if apply_coupon_code is not None and len(apply_coupon_code) > 0:
+        apply_coupon(o, apply_coupon_code)
+
+    if apply_debits_credits:
+        #apply taxes and discounts
+        apply_discounts(o)
+        apply_tax(o)
+
+    o.save()
+
     return jsonify({
         "status": "ok",
-        "orders": order_lis,
     })
-
-
-def post_order_status():
-    order_id = request.form.get("order_id")
-    status = request.form.get("status")
-    priv8_note = request.form.get("private_note")
-    pub_note = request.form.get("public_note")
-    redirect_url = request.form.get("redirect_to")
-
-    order_obj = orders.get(order_id)
-    order_obj.status = status
-    order_obj.status_public_notes = pub_note
-    order_obj.status_private_notes = priv8_note
-    orders.save(order_obj)
-
-    if redirect_url:
-        return render_template("redirect.html", **{
-            "redirect_url": redirect_url
-        })
-
-    return jsonify({"status": "ok"})
 
 
 def _register_api(app):
@@ -42,7 +54,4 @@ def _register_api(app):
     """
 
     app.add_url_rule('/order/',
-                     "get_order", get_order, methods=['GET'])
-
-    app.add_url_rule('/order/status/',
-                     "post_order_status", post_order_status, methods=['POST'])
+                     "put_order", put_order, methods=['PUT', 'POST'])
